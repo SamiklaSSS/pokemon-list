853e317afe5563c3760aaff3c34bc506a68b2328
SamiklaSSS <149561953+SamiklaSSS@users.noreply.github.com>
2024-02-28 10:12:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,100 +1,94 @@
-
 import csv
 
-pokemons = []
+apartments = []
 
 # https://www.w3schools.com/python/python_file_handling.asp
 # https://www.w3schools.in/python/file-handling
-with open('pokemon.csv', newline='') as csv_file:
-    file_reader = csv.reader(csv_file, delimiter = ',', quotechar='|')
+with open('jurmala.csv', newline='', encoding='utf-8') as csv_file:
+    file_reader = csv.reader(csv_file, delimiter = ',', quotechar='"')
 
     for row in file_reader:
-        pokemons.append(row[0])
+        apartments.append(row)
 
-# print(pokemons)
+# remove header row
+apartments.pop(0)
 
-print("Pokemon list command:")
+# print(apartments)
+
+n = 1 -2
+
+Ch = 0
+Ex = 0
+
+def Hprice (apartments):
+    323 > n
+    n = n +1
+    return apartments[n][8]
+
+def Lprice (apartments):
+    323 > n
+    n = n +1
+    return apartments[n][8]
+
+
 
 while True:
-    print("1. Get Pokemon by sequence number")
-    print("2. Sort by A-Z")
-    print("3. Sort by Z-A")
-    print("4. Search by text in name")
-    print("5. Search by length of name")
-    print("6. First 10 Pokemons")
-    print("7. Last 10 Pokemons")
-    print("8. List of 10 Pokemons") #nav pabeigta
-    print("9. Exit")
-    print("Please write everything with the big first letter!!!")
+    print("1. Get apartments by sequence number")
+    print("2. Top 10 by highest price")
+    print("3. Top 10 by lowest price")
+    print("4. 20 items, cheaper than price")
+    print("5. 20 items, expensive than price")
+    print("6. -- Filter of your choice --")
+    print("7. Exit")
 
-    choice = input("Enter your choice (1-9): ")
+    choice = input("Enter your choice (1-6): ")
 
     if choice == '1':
+        print(apartments)
         # https://www.w3schools.com/python/python_lists_access.asp
-        print(pokemons[int(input("Enter Pokemon's number!"))-1])
         pass
-    
     elif choice == '2':
+        apartments.sort(key = Hprice)
+        print(apartments[:10])
         # https://www.w3schools.com/python/python_lists_sort.asp
-        pokemons.sort()
-        print(pokemons)
         pass
-    
     elif choice == '3':
+        apartments.sort(key = Lprice)
+        print(apartments[-10:])
         # https://www.w3schools.com/python/python_lists_sort.asp
-        pokemons.reverse()
-        print(pokemons)
         pass
-    
     elif choice == '4':
+        Ch = input("Cheaper than what price, apartments are you searching for?")
+        newlistCh = []
+        for apartment in apartments:
+            323 > n
+            n = n +1
+            if Ch < apartment[n][8]:
+                newlistCh.append(apartment)
+        print(newlistCh[:20])
         # https://www.w3schools.com/python/python_lists_comprehension.asp
-        # https://www.w3schools.com/python/ref_string_startswith.asp
-        txt = input("Enter Pokemon's name or part of the name!")
-        newlist = []
-        for pokemon in pokemons:
-            if txt in pokemon:
-                newlist.append(pokemon)
-        print(newlist)
+        # https://www.w3schools.com/python/python_lists_access.asp - Range of Indexes
         pass
-    
     elif choice == '5':
+        Ex = input("More expencive than what price, apartments are you searching for?")
+        newlistEx = []
+        for apartment in apartments:
+            323 > n
+            n = n +1
+            if Ex > apartment[n][8]:
+                newlistEx.append(apartment)
+        print(newlistEx[-20:])
         # https://www.w3schools.com/python/python_lists_comprehension.asp
-        len_txt = input("Enter Pokemon's name's length!")
-        newlist = []
-        for pokemon in pokemons: 
-           if int(len_txt) == int(len(pokemon)):
-               newlist.append(pokemon)
-        print(newlist)
+        # https://www.w3schools.com/python/python_lists_access.asp - Range of Indexes
         pass
 
     elif choice == '6':
-        print(pokemons[:10])
+        print("Error - ???")
         pass
-
     elif choice == '7':
-        print(pokemons[-10:])
-        pass
-    
-    elif choice == '8': #nav pabeigta
-        a = 0
-        b = 10
-        b < 801
-        print(pokemons[a:b])
-        choice2 = input("If you want to continue the list enter 'n', if not enter 'q'!")
-        while choice2 == 'n':
-         input("You really want to conyinue the list?")
-         if choice2 == 'Yes':
-             a = a + 10
-             b = b + 10
-         print(pokemons[a:b])
-        pass
-
-    elif choice == '9':
         print("Exiting")
         break
-    
     else:
-        print("Invalid choice, choose from 1 to 8")
+        print("Invalid choice, choose from 1 to 7")
 
     print("==========================")
-
